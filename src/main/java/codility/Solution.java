7cd3d875ee2d5885385e041bdc53b3e47ea49c41
unknown <andrey.arnautov@.ua.akvelon.com>
2014-02-11 11:04:50 +0200

    init

---
+++ src/main/java/codility/Solution.java
@@ -0,0 +1,13 @@
+package codility;
+
+public class Solution {
+
+    int nMax = 90000;
+    int nMin = 0;
+
+    public int[] solution(int K, int[] T) {
+
+        return T;
+    }
+
+}
---
+++ src/test/java/codility/SolutionTest.java
@@ -0,0 +1,14 @@
+package codility;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        int[] result = solution.solution(0, null);
+
+    }
+}
